dfac3c88c658f5919f00500076cb0b32e9d63032
DeltaEcho192 <anthony@durrer.co.za>
2020-10-02 16:49:28 +0200

    Added persistant storage for user name. Added working Login API

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const fs = require('fs');
+const csv=require('csvtojson')
+
 app.get('/test/:id', (req, res) => {
   var jsonobj = require("./example.json");
   var trainStation = req.params.id;
@@ -20,6 +23,54 @@ app.get('/test/:id', (req, res) => {
 
 })
 
+app.get('/check/:userId', (req, res) => {
+  var usercheck = false;
+  emp.forEach(em => {
+    console.log(em._userId)
+    if(em._userId == req.params.userId)
+    {
+      console.log("Success")
+      usercheck = true;
+    }
+  });
+  console.log(req.params.userId);
+  console.log(usercheck);
+  res.send(JSON.stringify({"userid": req.params.userId ,"status": usercheck}));
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+class Employee {
+  set userId(userId)
+  {
+    this._userId=userId;
+  }
+  set Name(Name)
+  {
+    this._Name=Name;
+  }
+  set Password(Password)
+  {
+    this._Password=Password;
+  }
+  set Email(Email)
+  {
+    this._Email=Email;
+  }
+}
+let emp=[];
+
+// Invoking csv returns a promise
+const converter=csv()
+.fromFile('./users.csv')
+.then((json)=>{
+    let e;
+    json.forEach((row)=>{
+        e=new Employee();// New Employee Object
+        Object.assign(e,row);// Assign json to the new Employee
+        emp.push(e);// Add the Employee to the Array
+        
+    });
+});
--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
-
+import 'dart:io';
+import 'success.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
-Future<void> fetchUser(var userid) async {
+Future<Map<String, dynamic>> fetchUser(var userid) async {
   final response =
       await http.get('http://192.168.202.107:3000/check/' + userid.toString());
 
@@ -13,8 +15,8 @@ Future<void> fetchUser(var userid) async {
     // then parse the JSON.
     //print(json.decode(response.body));
 
-    var logIn = jsonDecode(response.body);
-    print(logIn);
+    Map<String, dynamic> logIn = jsonDecode(response.body);
+    return logIn;
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
@@ -22,41 +24,120 @@ Future<void> fetchUser(var userid) async {
   }
 }
 
-void main() => runApp(MyApp());
+class UserStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/user.txt');
+  }
+
+  Future<String> readUser() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+
+      return contents;
+    } catch (e) {
+      // If encountering an error, return 0
+      return 'error';
+    }
+  }
+
+  Future<File> writeUser(String userid) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$userid');
+  }
+}
+
+void main() => runApp(
+      MaterialApp(
+          title: "Login In testing", home: MyApp(storage: UserStorage())),
+    );
 
 class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
+  final UserStorage storage;
+  MyApp({Key key, @required this.storage}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Future<Map<String, dynamic>> logStatus;
+  String _user;
+
   @override
   void initState() {
     super.initState();
-    fetchUser('ad');
+    widget.storage.readUser().then((value) => {
+          print(value),
+          setState(() {
+            _user = value;
+          })
+        });
+    logStatus = fetchUser('ad');
+  }
+
+  Future<File> _writeUser(String userid) {
+    setState(() {
+      _user = userid;
+    });
+
+    return widget.storage.writeUser(userid);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text('Fetch Data Example'),
         ),
-        body: Center(
-            child: RaisedButton(
-          onPressed: () {
-            fetchUser("ad");
-          },
-          child: new Text("Execute"),
-        )),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topCenter,
+              child: RaisedButton(
+                onPressed: () {
+                  fetchUser('ad').then((value) => {
+                        print(value['userid']),
+                        if (value['status'] == true)
+                          {
+                            print("Login was successful"),
+                            _writeUser(value['userid']),
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HelloWorld()),
+                            )
+                          }
+                        else
+                          {print("Login was unsuccessful")}
+                      });
+                },
+                child: new Text("Execute"),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: RaisedButton(
+                onPressed: () {
+                  widget.storage.readUser().then((value) => {
+                        print(value),
+                      });
+                },
+                child: new Text("Read"),
+              ),
+            ),
+          ],
+        ));
   }
 }
